81513c424186b5556808eb1e61d3b464c4cdd7e5
sbkwgh <5166028+sbkwgh@users.noreply.github.com>
2019-09-30 18:36:32 +0100

    Add settings panel

--- main.js
+++ main.js
@@ -1,45 +1,103 @@
-const HEIGHT = 500;
-const WIDTH = 1000;
-const N_POINTS = 10;
-const SIZE = 0.5;
+//Settings
+const settings = {
+	height: 500,
+	width: 1000,
+	n_points: 100,
+	size: 0.5,
+	translate_dist: 2,
+	bearing_rand_deviation: degToRad(180),
+	sin_half_period: 5,
+	tick_duration: 10,
+	clear_on_draw: false,
+	reset_seconds: 5,
+	mouse_control: false,
 
-const TRANSLATE_DIST = 2;
-const BEARING_RAND_DEVIATION = degToRad(180);
-const SIN_HALF_PERIOD = 5;
+	_mouseLocation: null
+};
 
-const TICK_DURATION = 10;
-const CLEAR_ON_DRAW = false;
-const RESET_SECONDS = 5;
-const MOUSE_CONTROL = false;
+//Event handlers
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 
-const $canvas = document.querySelector('canvas');
-const ctx = $canvas.getContext('2d');
+$('body').addEventListener('mousemove', function (e) {
+	settings._mouseLocation = new Point(e.pageX, e.pageY, []);
+});
+$('#settings').addEventListener('click', function () {
+	const settingsNames = [
+		'size',
+		'n_points',
+		'bearing_rand_deviation',
+		'sin_half_period',
+		'clear_on_draw',
+		'reset_seconds',
+		'mouse_control'
+	];
+	for (key of settingsNames) {
+		let val = settings[key];
+		let $input = $(`input[name=${key}]`);
 
-$canvas.setAttribute('height', HEIGHT);
-$canvas.setAttribute('width', WIDTH);
+		if (typeof val === 'boolean') {
+			$input.checked = val;
+		} else if (key === 'bearing_rand_deviation') {
+			$input.value = radToDeg(val);
+		} else {
+			$input.value = val;
+		}
+	}
 
-ctx.fillStyle = '#000';
-ctx.fillRect(0, 0, WIDTH, HEIGHT);
+	$('#modal_container').classList.toggle('hidden');
+});
+$('#modal_container').addEventListener('click', function (e) {
+	if (e.target === this) {
+		this.classList.toggle('hidden');
+	}
+});
+$('form').addEventListener('submit', function (e) {
+	e.preventDefault();
 
+	const $inputs = $$('form input');
+	for (let $input of $inputs) {
+		const name = $input.name;
+		const value = +$input.value;
+		
+		if($input.type === 'checkbox') {
+			settings[name] = $input.checked;
+		} else if (name === 'bearing_rand_deviation') {
+			settings[name] = degToRad(value);
+		} else {
+			settings[name] = value;
+		}
+
+		if (name === 'n_points') {
+			createPoints();
+		}
+	}
+	
+	clearCanvas();
+	
+	$('#modal_container').classList.toggle('hidden');
+});
+$('#reset').addEventListener('click', function () {
+	location.href = location.href;
+});
+
+//Utility functions
 function rand (min, max, round) {
 	let a = min + Math.random() * (max-min);
 
 	return round ? Math.round(a) : a;
 }
-
 function randSign () {
 	return rand(0, 1, true) ? -1 : 1;
 }
-
 function degToRad (deg) {
 	return Math.PI * deg/180;
 }
+function radToDeg (rad) {
+	return 180 * rad/Math.PI;
+}
 
-let mouseLocation = null;
-document.body.addEventListener('mousemove', e => {
-	mouseLocation = new Point(e.pageX, e.pageY, []);
-});
-
+//Point mutation function
 function mutate (_point, tick) {
 	const point = new Point(
 		_point.x,
@@ -47,7 +105,7 @@ function mutate (_point, tick) {
 		_point.collection
 	);
 
-	const nearest = point.getNNearest(N_POINTS).map(p => [p.x, p.y]);
+	const nearest = point.getNNearest(settings.n_points).map(p => [p.x, p.y]);
 	const summedCoords = nearest.reduce((acc, c) => {
 		acc[0] += c[0];
 		acc[1] += c[1];
@@ -60,11 +118,13 @@ function mutate (_point, tick) {
 	};
 	const averageBearing = point.getBearing(averageCoord);
 
-	const magnitude = TRANSLATE_DIST * Math.sin((tick*Math.PI) / (1000/TICK_DURATION*SIN_HALF_PERIOD));
-	const randDeviation = rand(0, BEARING_RAND_DEVIATION);
+	const magnitude =
+		settings.translate_dist *
+		Math.sin((tick*Math.PI) /(1000/settings.tick_duration*settings.sin_half_period));
+	const randDeviation = rand(0, settings.bearing_rand_deviation);
 
-	if (mouseLocation && MOUSE_CONTROL) {
-		const mouseBearing = point.getBearing(mouseLocation);
+	if (settings._mouseLocation && settings.mouse_control) {
+		const mouseBearing = point.getBearing(settings._mouseLocation);
 		const finalBearing = (averageBearing + mouseBearing) / 2; 
 
 		point.translateInDir(magnitude, finalBearing + randDeviation);
@@ -75,6 +135,7 @@ function mutate (_point, tick) {
 	return point;
 }
 
+//Class definitions
 class Point {
 	constructor (x, y, collection) {
 		this.x = x;
@@ -133,22 +194,41 @@ class Point {
 	}
 }
 
+//Main
+const $canvas = $('canvas');
+$canvas.setAttribute('height', settings.height);
+$canvas.setAttribute('width', settings.width);
 
-let points = [];
-for (let i = 0; i < N_POINTS; i++) {
-	const point = new Point(
-		rand(0, WIDTH), rand(0, HEIGHT), points
-	);
-	points.push(point);
+const ctx = $canvas.getContext('2d');
+ctx.fillStyle = '#000';
+ctx.fillRect(0, 0, settings.width, settings.height);
+
+function clearCanvas () {
+	ctx.fillStyle = '#000';
+	ctx.fillRect(0, 0, settings.width, settings.height);
+}
+
+let points;
+function createPoints () {
+	points = [];
+
+	for (let i = 0; i < settings.n_points; i++) {
+		const point = new Point(
+			rand(0, settings.width), rand(0, settings.height), points
+		);
+		points.push(point);
+	}
 }
 
 let tick = 0;
 function main () {
 	tick++;
 	
-	if (CLEAR_ON_DRAW || tick % (RESET_SECONDS * 1000/TICK_DURATION) === 0) {
-		ctx.fillStyle = '#000';
-		ctx.fillRect(0, 0, WIDTH, HEIGHT);
+	if (
+		settings.clear_on_draw ||
+		tick % (settings.reset_seconds * 1000/settings.tick_duration) === 0
+	) {
+		clearCanvas();
 	}
 	
 	let mutatedPoints = [];
@@ -158,10 +238,12 @@ function main () {
 		
 		ctx.beginPath();
 		ctx.fillStyle = '#fff';
-		ctx.arc(mutatedPoint.x, mutatedPoint.y, SIZE, 0, Math.PI*2);
+		ctx.arc(mutatedPoint.x, mutatedPoint.y, settings.size, 0, Math.PI*2);
 		ctx.fill();
 	}
 
 	points = mutatedPoints;
 }
-setInterval(main, TICK_DURATION);
+
+createPoints();
+setInterval(main, settings.tick_duration);
